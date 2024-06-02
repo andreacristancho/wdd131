93ce99893f219788d5c50347ccb36ed358a82fd9
andreacristancho <andreacristancho01@gmail.com>
2024-06-01 21:38:06 -0500

    Project Version 3 Jun 1 9:37pm

--- scripts/project.js
+++ scripts/project.js
@@ -21,3 +21,27 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   
   
+  function evaluateForm() {
+    const experience = parseInt(document.getElementById('experience').value);
+    const engagement = parseInt(document.getElementById('engagement').value);
+    let score = 0;
+
+    // Asignar puntaje basado en la experiencia
+    score += experience;
+
+    // Asignar puntaje basado en el compromiso
+    score += engagement;
+
+    // Mostrar el resultado
+    const result = document.getElementById('result');
+    result.textContent = `Your score is: ${score}`;
+
+    // Acciones basadas en el puntaje
+    if (score <= 2) {
+        result.textContent += " This profile indicates a basic profile. You are starting out and have an initial interest in developing your skills and knowledge in the area. You will have to dedicate time and effort to achieve the results you desire";
+    } else if (score <= 4) {
+        result.textContent += " This profile reflects an intermediate profile. With a good understanding of the key practices and processes, you will be able to move interesting projects forward. ";
+    } else {
+        result.textContent += " This profile shows a high, highlighting your ability to lead and apply advanced knowledge in the field. Take advantage of your experience and availability to carry out large-scale projects.";
+    }
+}
